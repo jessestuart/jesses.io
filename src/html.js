1e6e05ca187fded13915b6e952176690d2e82ad8
Jesse Stuart <jdstuart@icloud.com>
2017-12-13 14:24:09 -0500

    [commit message skipped]

--- src/html.js
+++ src/html.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 let stylesStr
 if (process.env.NODE_ENV === 'production') {
@@ -9,57 +9,60 @@ if (process.env.NODE_ENV === 'production') {
   }
 }
 
-class HTML extends Component {
-  render() {
-    let css
-    if (process.env.NODE_ENV === 'production') {
-      css = (
-        <style
-          id="gatsby-inlined-css"
-          dangerouslySetInnerHTML={{ __html: stylesStr }}
+const css =
+  process.env.NODE_ENV === 'production' ? (
+    <style
+      id="gatsby-inlined-css"
+      dangerouslySetInnerHTML={{ __html: stylesStr }}
+    />
+  ) : null
+
+const description = 'Jesse has things to say.'
+
+export default function HTML(props) {
+  const {
+    body,
+    bodyAttributes,
+    headComponents,
+    htmlAttributes,
+    postBodyComponents,
+    preBodyComponents,
+  } = props
+
+  return (
+    <html {...htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-      )
-    }
 
-    const description = 'Jesse has things to say.'
+        <meta name="description" content={description} />
 
-    return (
-      <html {...this.props.htmlAttributes}>
-        <head>
-          <meta charSet="utf-8" />
-          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@jesse_stuart" />
+        <meta name="twitter:creator" content="@jesse_stuart" />
 
-          <meta name="description" content={description} />
+        <meta property="og:description" content={description} />
+        <meta property="og:site_name" content="Jesse Stuart" />
+        <meta property="og:type" content="article" />
 
-          <meta name="twitter:card" content="summary" />
-          <meta name="twitter:site" content="@jesse_stuart" />
-          <meta name="twitter:creator" content="@jesse_stuart" />
+        <meta itemProp="description" content={description} />
 
-          <meta property="og:description" content={description} />
-          <meta property="og:site_name" content="Jesse Stuart" />
-          <meta property="og:type" content="article" />
-
-          <meta itemProp="description" content={description} />
-
-          {this.props.headComponents}
-          {css}
-        </head>
-        <body {...this.props.bodyAttributes}>
-          {this.props.preBodyComponents}
-          <div
-            key={'body'}
-            id="___gatsby"
-            dangerouslySetInnerHTML={{ __html: this.props.body }}
-          />
-          {this.props.postBodyComponents}
-        </body>
-      </html>
-    )
-  }
+        {headComponents}
+        {css}
+      </head>
+      <body {...bodyAttributes}>
+        {preBodyComponents}
+        <div
+          key={'body'}
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: body }}
+        />
+        {postBodyComponents}
+      </body>
+    </html>
+  )
 }
-
-module.exports = HTML
