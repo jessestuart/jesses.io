e208fe8196775925b45791a7a233f5104d72c12f
Jesse Stuart <jdstuart@icloud.com>
2017-12-28 13:53:25 -0500

    [ci] Make `setup-puppeteer` script executable.

--- src/components/ProfileBlurb/ProfileBlurb.js
+++ src/components/ProfileBlurb/ProfileBlurb.js
@@ -1,6 +1,12 @@
 import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 
+const colingUrl =
+  'https://f001.backblazeb2.com/file/js-public/Dimensions%20of%20Variation%20in%20American%20English%20(COLING%2C%202014).pdf'
+const uspoUrl = 'https://f001.backblazeb2.com/file/js-public/US20150026079.pdf'
+const beigbederUrl =
+  'https://www.scribd.com/doc/184481594/Love-Lasts-Three-Years'
+
 const ProfileBlurb = () => (
   <Fragment>
     <ProfileBlurbHeading />
@@ -8,13 +14,14 @@ const ProfileBlurb = () => (
       NYC-based software engineer.
     </SubBlurb>
     <SubBlurb emoji1="📖" emoji2="📜">
-      Published NLP researcher & <br /> USPO patent holder.
+      <a href={colingUrl}>Published </a>&nbsp;NLP researcher;<br />
+      USPO <a href={uspoUrl}>Patent holder</a>.
     </SubBlurb>
     <SubBlurb emoji1="🔬" emoji2="🐀">
       Neuroscientist by degree.
     </SubBlurb>
     <SubBlurb emoji1="🇫🇷" emoji2="🌙">
-      French translator by night.
+      French <a href={beigbederUrl}>translator</a> by night.
     </SubBlurb>
   </Fragment>
 )
@@ -24,7 +31,15 @@ const ProfileBlurbHeading = () => (
     <h2 className="f3-ns f4 fw5 lh-title">Hi. I'm Jesse.</h2>
     <h2 className="f3-ns f4 fw5 lh-title mv3">
       <span className="dib nowrap">I contradict myself.</span>
-      <span className="dib nowrap ml2">I contain multitudes.</span>
+      <span className="dib nowrap ml2">
+        I contain&nbsp;
+        <a
+          className="link moon-gray"
+          href="http://www.english.illinois.edu/maps/poets/s_z/whitman/song.htm"
+        >
+          multitudes.
+        </a>
+      </span>
     </h2>
   </Fragment>
 )
