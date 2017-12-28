c17e8ac9153eb78479567b0f6027b60de6688e7b
Jesse Stuart <jdstuart@icloud.com>
2017-12-28 10:38:55 -0500

    [commit message skipped]

--- src/components/SiteHeader/index.js
+++ src/components/SiteHeader/index.js
@@ -1,17 +1,51 @@
-import React from 'react'
+import React, { Fragment, Component } from 'react'
 import _ from 'lodash'
 import Link from 'gatsby-link'
+import classNames from 'classnames'
 
 import config from '../../../gatsby-config'
+import '../../styles/hamburgers/hamburgers.scss'
 
-const SiteHeader = () => (
-  <div className="bb b--hot-pink">
-    <h3 className="lato fw7 f3 pa3">
-      <Link className="color-inherit no-underline shadow-none" to={'/'}>
-        {_.get(config, 'siteMetadata.title')}
-      </Link>
-    </h3>
-  </div>
-)
+class SiteHeader extends Component {
+  constructor() {
+    super()
+    this.initialState = {
+      isHamburgerMenuEnabled: false,
+    }
+  }
+
+  toggleHamburgerMenu() {
+    console.log('toggle')
+    this.setState({
+      isHamburgerMenuEnabled: !this.state.isHamburgerMenuEnabled,
+    })
+  }
+
+  render() {
+    const isHamburgerMenuEnabled = this.state
+    return (
+      <Fragment>
+        <div className="bb b--hot-pink">
+          <h3 className="lato fw7 f3 pa3">
+            <Link className="color-inherit no-underline shadow-none" to={'/'}>
+              {_.get(config, 'siteMetadata.title')}
+            </Link>
+          </h3>
+        </div>
+        <button
+          className={classNames('hamburger hamburger--collapse', {
+            'is-active': isHamburgerMenuEnabled,
+          })}
+          onClick={this.toggleHamburgerMenu}
+          type="button"
+        >
+          <span className="hamburger-box">
+            <span className="hamburger-inner" />
+          </span>
+        </button>
+      </Fragment>
+    )
+  }
+}
 
 export default SiteHeader
