7d4e8228e60a6dd781c6136fa0546036d259923f
Jesse Stuart <jdstuart@icloud.com>
2017-12-28 10:17:24 -0500

    [commit message skipped]

--- __tests__/home-test.js
+++ __tests__/home-test.js
@@ -7,7 +7,7 @@ describe('/ (Home page)', () => {
     await page.goto('localhost:8000')
   }, timeout)
 
-  afterAll(async () => global.__BROWSER__.close())
+  // afterAll(async () => global.__BROWSER__.close())
 
   it('Should load without error.', async () => {
     let text = await page.evaluate(() => document.body.textContent)
@@ -18,6 +18,6 @@ describe('/ (Home page)', () => {
     const screenshot = await page.screenshot({
       path: '__snapshots__/home-page.png',
     })
-    expect(screenshot)
+    expect(screenshot).toBeDefined()
   })
 })
