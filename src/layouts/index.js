0b584acc18dfed12e1a79199532e9ab59d8f1e15
Jesse Stuart <jdstuart@icloud.com>
2018-01-03 06:48:38 -0500

    [tests] Update jest screenshot.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,21 +1,20 @@
-// import React from 'react'
-//
-// import { SiteHeader } from '../components'
-//
-// import 'prismjs/themes/prism.css'
-//
-// const Template = ({ children, location }) => (
-//   <div
-//     style={{
-//       display: 'grid',
-//       gridGap: '1vw',
-//       gridTemplateColumns: 'repeat(10, 9vw)',
-//     }}
-//   >
-//     <SiteHeader />
-//     <div>foo</div>
-//     <div style={{ gridColumn: '3 / 9' }}>{children()}</div>
-//   </div>
-// )
-//
-// export default Template
+import React from 'react'
+
+import { SiteHeader } from '../components'
+
+import 'prismjs/themes/prism.css'
+
+const Template = ({ children, location }) => (
+  <div
+    style={{
+      display: 'grid',
+      gridGap: '1vw',
+      gridTemplateColumns: 'repeat(10, 9vw)',
+    }}
+  >
+    <SiteHeader />
+    <div style={{ gridColumn: '3 / 9' }}>{children()}</div>
+  </div>
+)
+
+export default Template
