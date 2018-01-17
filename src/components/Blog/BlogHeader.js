76badf6661a25c4f619a29d07496e145136ca8b9
Jesse Stuart <hi@jessestuart.com>
2018-01-16 21:33:47 -0500

    [ui] Home page + blog post template UI tweaks.

--- src/components/Blog/BlogHeader.js
+++ src/components/Blog/BlogHeader.js
@@ -1,15 +1,13 @@
 import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import { Calendar } from 'react-feather'
 
 const BlogHeader = ({ date, slug, title }) => {
-  console.log({ date, slug, title })
   return (
     <Fragment>
       <h2 className="mb2 mt4 f3 fw7 underline header-purple">
-        <Link style={{ boxShadow: 'none' }} to={slug}>
-          {title}
-        </Link>
+        <Link to={slug}>{title}</Link>
       </h2>
       <p className="f6 mv2 flex items-center">
         <Calendar className="header-purple mr2" size="1rem" />
@@ -17,6 +15,12 @@ const BlogHeader = ({ date, slug, title }) => {
       </p>
     </Fragment>
   )
+}
+
+BlogHeader.propTypes = {
+  date: PropTypes.string.isRequired,
+  slug: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
 }
 
 export default BlogHeader
--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,13 +1,14 @@
 import React, { Component, Fragment } from 'react'
-import get from 'lodash/get'
 import Helmet from 'react-helmet'
+import _ from 'lodash'
 
 import { BlogHeader } from '../components/Blog'
 
 class BlogIndex extends Component {
   render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const posts = get(this, 'props.data.allMarkdownRemark.edges')
+    const { props } = this
+    const siteTitle = _.get(props, 'data.site.siteMetadata.title')
+    const posts = _.get(props, 'data.allMarkdownRemark.edges')
     if (!posts) {
       return <div />
     }
@@ -15,19 +16,15 @@ class BlogIndex extends Component {
     return (
       <Fragment>
         <Helmet title={siteTitle} />
-        {posts.map(({ node }, index) => {
-          const title = get(node, 'frontmatter.title') || node.fields.slug
+        {posts.map(({ node }) => {
+          const { excerpt, fields, frontmatter } = node
+          const date = _.get(frontmatter, 'date')
+          const slug = _.get(fields, 'slug')
+          const title = _.get(frontmatter, 'title')
           return (
-            <div key={node.fields.slug}>
-              <BlogHeader
-                date={node.frontmatter.date}
-                slug={node.fields.slug}
-                title={title}
-              />
-              <p
-                className="f4"
-                dangerouslySetInnerHTML={{ __html: node.excerpt }}
-              />
+            <div key={slug}>
+              <BlogHeader date={date} slug={slug} title={title || slug} />
+              <p className="f4" dangerouslySetInnerHTML={{ __html: excerpt }} />
             </div>
           )
         })}
