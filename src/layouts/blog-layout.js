ea50adb944226b760a51b858caecd2e327aa9cfa
Jesse Stuart <hi@jessestuart.com>
2018-01-21 07:18:01 -0500

    [misc updates]

--- src/layouts/blog-layout.js
+++ src/layouts/blog-layout.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
 
 import { SiteHeader, SiteFooter } from '../components'
 
@@ -8,14 +9,19 @@ const BlogLayout = ({ children, location }) => {
   return (
     <Fragment>
       <SiteHeader location={location} />
-      <div className="bg-near-white black-80 flex justify-center flex-body-expand">
-        <div className="mw6 mw7-ns lh-copy w-75 w-50-ns center">
-          {children()}
-        </div>
+      <div className="bg-near-white black-80 flex justify-center flex-body-expand pv5">
+        <div className="mw6 mw7-ns lh-copy w-75 center">{children()}</div>
       </div>
       <SiteFooter theme={SiteFooter.Theme.Dark} />
     </Fragment>
   )
+}
+
+BlogLayout.propTypes = {
+  children: PropTypes.any,
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired,
+  }),
 }
 
 export default BlogLayout
