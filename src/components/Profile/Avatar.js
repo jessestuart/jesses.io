587d33d1d76af5ddb663f3741cb931b0a0301aec
Jesse Stuart <hi@jessestuart.com>
2018-02-02 22:12:01 -0500

    [ui,cleanup] Update avatar to use gatsby-image.

--- src/components/Profile/Avatar.js
+++ src/components/Profile/Avatar.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import classNames from 'classnames'
+import Img from 'gatsby-image'
 
-import avatarImage from '../../../static/img/avatar_square.jpg'
-
-const Avatar = () => (
-  <img
-    className={classNames('br-100 flex justify-center mv2 pa0 shadow-6')}
-    src={avatarImage}
+const Avatar = ({ image }) => (
+  <Img
+    className={classNames('br-100 justify-center mv2 pa0 shadow-6')}
+    sizes={image.sizes}
   />
 )
 
 export default Avatar
+
+export const query = graphql`
+  fragment avatar on RootQueryType {
+    file(name: { eq: "avatar-square" }) {
+      childImageSharp {
+        sizes {
+          ...GatsbyImageSharpSizes
+        }
+      }
+    }
+  }
+`
