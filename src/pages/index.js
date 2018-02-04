f602c45dd477c845dad7683fa97eb1b000b0557b
Jesse Stuart <hi@jessestuart.com>
2018-02-04 07:35:53 -0500

    [ui] Misc updates.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,35 @@
-import React from 'react'
-import Helmet from 'react-helmet'
+/* @flow */
+import React, { Fragment } from 'react'
 import { ProfileDevIcons, Profile } from '../components'
-import _ from 'lodash'
 
-import config from '../../gatsby-config'
-
-import 'prismjs/themes/prism.css'
-import '../styles/base.css'
-
-const Home = ({ data }) => {
-  const title = _.get(config, 'siteMetadata.title')
-  return (
+const Home = ({ data }: Props) => (
+  <Fragment>
     <div className="moon-gray bg-gray-primary">
-      <Helmet title={title}>
-        <meta itemProp="name" content={title} />
-        <meta name="twitter:title" content={title} />
-        <meta property="og:title" content={title} />
-        <meta property="og:url" content={config.siteMetadata.siteUrl} />
-      </Helmet>
-      <Profile avatar={_.get(data, 'file.childImageSharp')} />
+      <Profile avatar={data.file.childImageSharp} />
       <ProfileDevIcons />
     </div>
-  )
+  </Fragment>
+)
+
+type Props = {
+  data: {
+    file: {
+      childImageSharp: {
+        sizes: {
+          aspectRatio: Number,
+          base64: String,
+          sizes: String,
+          src: String,
+          srcSet: String,
+        },
+      },
+    },
+  },
 }
 
 export default Home
 
+// $FlowFixMe
 export const query = graphql`
   query AvatarImageQuery {
     file(name: { eq: "avatar-square" }) {
