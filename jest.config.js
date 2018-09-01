fcaf0fe4fdba0c966f5fe4892a324fa193a6933c
Jesse Stuart <hi@jessestuart.com>
2018-09-01 19:51:49 -0400

    [cleanup]

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,5 @@
 module.exports = {
-  moduleFileExtensions: ['ts', 'tsx', 'js'],
-  transform: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/test/utils/file-transformer.js',
-    '^.+\\.(js)$': 'babel-jest',
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
+  coverageDirectory: './test/coverage',
   globals: {
     __PATH_PREFIX__: true,
     __BROWSER__: true,
@@ -14,24 +8,17 @@ module.exports = {
       tsConfigFile: '<rootDir>/tsconfig.json',
     },
   },
-  testMatch: ['<rootDir>/**/**/*.spec.+(ts|tsx)'],
-  coverageDirectory: './test/coverage',
   moduleDirectories: ['node_modules', 'src', 'static'],
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
   moduleNameMapper: {
     '\\.(s)?css$': '<rootDir>/test/__mocks__/styleMock.js',
   },
+  testMatch: ['<rootDir>/**/**/*.spec.+(ts|tsx)'],
   testPathIgnorePatterns: ['/node_modules/', '<rootDir>/.cache/', '/public/'],
-  // ==========================
-  // transform: {
-  //   '^.+\\.js$': 'babel-jest',
-  // },
-  // moduleNameMapper: {
-  //   '\\.(s)?css$': '<rootDir>/test/__mocks__/styleMock.js',
-  // },
-  // testPathIgnorePatterns: ['/node_modules/', '<rootDir>/.cache/'],
-  // globals: {
-  //   __PATH_PREFIX__: true,
-  //   __BROWSER__: true,
-  //   graphql: true,
-  // },
+  transform: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/test/utils/file-transformer.js',
+    '^.+\\.(js)$': 'babel-jest',
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
 }
