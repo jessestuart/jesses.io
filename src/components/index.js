5c5ef80328427cf33748cac0baf55804c9776952
Jesse Stuart <hi@jessestuart.com>
2018-09-02 15:30:18 -0400

    [commit message skipped]

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export Profile from './Profile/Profile'
-export ProfileDevIcons from './ProfileDevIcons'
-export SiteFooter from './SiteFooter/SiteFooter'
-export SiteHeader from './SiteHeader/SiteHeader'
