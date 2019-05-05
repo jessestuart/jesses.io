cd324f2a23fa8c9fab8b2c15a3e9819671ab7dc7
Jesse Stuart <hi@jessestuart.com>
2019-05-05 02:55:13 -0400

    feat(tests): Store jest reports in circleci.
    Signed-off-by: Jesse Stuart <hi@jessestuart.com>

--- src/components/Profile/Profile.spec.tsx
+++ src/components/Profile/Profile.spec.tsx
@@ -4,6 +4,10 @@ import renderer from 'react-test-renderer'
 import Profile from './Profile'
 
 describe('Render Profile component.', () => {
+  beforeAll(() => {
+    jest.useFakeTimers()
+  })
+
   test('Render Profile component.', () => {
     const component = renderer.create(<Profile href="/" />)
     const tree = component.toJSON()
