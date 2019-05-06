de8d3587e9b501993755f4fec728ee255697455e
Jesse Stuart <hi@jessestuart.com>
2019-05-06 16:25:23 -0400

    feat(ci): Add automated Sentry.io deployment / commit tracking.
    Signed-off-by: Jesse Stuart <hi@jessestuart.com>

---
+++ scripts/update-sentry-release.sh
@@ -0,0 +1,11 @@
+export SENTRY_AUTH_TOKEN=$SENTRY_AUTH_TOKEN
+export SENTRY_ORG=jesses
+VERSION=$(sentry-cli releases propose-version)
+
+# Create a release
+sentry-cli releases new -p jesses-io $VERSION
+
+# Associate commits with the release
+sentry-cli releases set-commits --auto $VERSION
+
+sentry-cli releases deploys $VERSION new -e Production
