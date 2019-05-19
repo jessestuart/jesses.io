54cf1e0475a01a1206ef75a460c021a50db2f4e3
Jesse Stuart <hi@jessestuart.com>
2019-05-18 21:37:16 -0400

    chore(scripts): Add shebang.
    Signed-off-by: Jesse Stuart <hi@jessestuart.com>

--- scripts/update-sentry-release.sh
+++ scripts/update-sentry-release.sh
@@ -1,11 +1,12 @@
-export SENTRY_AUTH_TOKEN=$SENTRY_AUTH_TOKEN
+#!/bin/sh
 export SENTRY_ORG=jesses
+
 VERSION=$(sentry-cli releases propose-version)
 
 # Create a release
-sentry-cli releases new -p jesses-io $VERSION
+sentry-cli releases new -p jesses-io "$VERSION"
 
 # Associate commits with the release
-sentry-cli releases set-commits --auto $VERSION
+sentry-cli releases set-commits --auto "$VERSION"
 
-sentry-cli releases deploys $VERSION new -e Production
+sentry-cli releases deploys "$VERSION" new -e Production
